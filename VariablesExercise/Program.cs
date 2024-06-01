cb1a95fcb5e5c1afc03c49dbe3d3963fe6ee5516
Mohammad Jabir <mohammadj94@gmail.com>
2024-05-31 23:49:51 -0600

    completed exercise

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace VariablesExercise
+﻿using System.Diagnostics;
+
+namespace VariablesExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            string stringVariable;
+            int intVariable;
+            char charVariable;
+            bool boolVariable;
+            double doubleVariable;
+            decimal decimalVariable;
+
+            Console.Write("What is your pet's name: ");
+            stringVariable = Console.ReadLine();
+
+            Console.Write("How old is your pet: ");
+            intVariable = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Your pet's name is {stringVariable} and it is {intVariable} years old.");
         }
     }
 }
